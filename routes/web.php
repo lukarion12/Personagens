f5a3e8e25600457789374cc68a683b5b74c676da
Cisco <cisco@senaiescola.fsa>
2018-11-06 21:31:00 -0200

    sobrescrito
    colado

--- routes/web.php
+++ routes/web.php
@@ -14,23 +14,3 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/', function(){
-    return view('principal');
-});
-Route::get('/categorias', 'CategoriaController@index');
-Route::get('/categorias/nova', 'CategoriaController@create');
-Route::get('/categorias/apagar/{id}', 'CategoriaController@destroy');
-Route::get('/categorias/editar/{id}', 'CategoriaController@edit');
-Route::post('/categorias/editar/{id}', 'CategoriaController@update');
-Route::post('/categorias', 'CategoriaController@store');
-Route::get('/categorias/restaurar', 'CategoriaController@indexWithTrashed');
-Route::get('/categorias/restaurar/{id}', 'CategoriaController@restore');
-
-Route::get('/produtos', 'ProdutoController@index');
-Route::get('/produtos/novo', 'ProdutoController@create');
-Route::get('/produtos/editar/{id}', 'ProdutoController@edit');
-Route::get('/produtos/apagar/{id}', 'ProdutoController@destroy');
-Route::post('/produtos','ProdutoController@store');
-
-Route::post('/produtos/editar/{id}','ProdutoController@update');
